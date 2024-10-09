43fd677986aad3c8f05ab9e5ebfac88eb5746e58
BairM1247 <mba84@mail.ru>
2024-10-09 20:57:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+def password(num):
+    pass_ = ''
+    for i in range(1, num):
+        for j in range(i + 1, num):
+            if num % (i + j) == 0:
+                pass_ += str(i) + str(j)
+    return pass_
+
+print('Введите любое число: ')
+print(password(int(input())))
